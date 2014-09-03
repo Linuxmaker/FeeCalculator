a2f7201897019878a8abb6cc52b2c3ef0a27cdf5
Andreas Günther <git@linuxmaker.com>
2014-09-03 19:13:01 +0200

    Extend the class to check internet connection.

--- src/com/linuxmaker/calculator/Parser.java
+++ src/com/linuxmaker/calculator/Parser.java
@@ -4,9 +4,11 @@
 package com.linuxmaker.calculator;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.swing.JOptionPane;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -27,14 +29,16 @@ public class Parser {
 	public double parseduration(String start, String destination) throws SAXException, IOException, XPathExpressionException, ParserConfigurationException {
 		double duration = 0;
 		try {
-			URL myUrl = new URL("http://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + start + "&destinations=" + destination + "&mode=driving&language=de-DE&sensor=false");
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document document = builder.parse(myUrl.openStream());
-			XPathFactory xPathfactory = XPathFactory.newInstance();
-			XPath xpath = xPathfactory.newXPath();
-			XPathExpression expression = xpath.compile("/DistanceMatrixResponse/row/element/duration/value");
-			duration = (Double.valueOf(expression.evaluate(document, XPathConstants.STRING).toString()))/3600;
+			if (connectInternet()) {
+				URL myUrl = new URL("http://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + start + "&destinations=" + "\"" + destination + "\""  + "&mode=driving&language=de-DE&sensor=false");
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				Document document = builder.parse(myUrl.openStream());
+				XPathFactory xPathfactory = XPathFactory.newInstance();
+				XPath xpath = xPathfactory.newXPath();
+				XPathExpression expression = xpath.compile("/DistanceMatrixResponse/row/element/duration/value");
+				duration = (Double.valueOf(expression.evaluate(document, XPathConstants.STRING).toString()))/3600;
+			}
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
@@ -44,18 +48,38 @@ public class Parser {
 	public double parsedistance(String start, String destination) throws SAXException, IOException, XPathExpressionException {
 		double distance = 0;
 		try {
-			URL myUrl = new URL("http://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + start + "&destinations=" + destination + "&mode=driving&language=de-DE&sensor=false");
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document document = builder.parse(myUrl.openStream());
-			XPathFactory xPathfactory = XPathFactory.newInstance();
-			XPath xpath = xPathfactory.newXPath();
-			XPathExpression expression = xpath.compile("/DistanceMatrixResponse/row/element/distance/value");
-			distance = (Double.valueOf(expression.evaluate(document, XPathConstants.STRING).toString()))/1000;
+			if (connectInternet()) {
+				URL myUrl = new URL("http://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + start + "&destinations=" + "\"" + destination + "\"" + "&mode=driving&language=de-DE&sensor=false");
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				Document document = builder.parse(myUrl.openStream());
+				XPathFactory xPathfactory = XPathFactory.newInstance();
+				XPath xpath = xPathfactory.newXPath();
+				XPathExpression expression = xpath.compile("/DistanceMatrixResponse/row/element/distance/value");
+				distance = (Double.valueOf(expression.evaluate(document, XPathConstants.STRING).toString()))/1000;
+			}
 		} catch (ParserConfigurationException e) {
 			System.out.println("Der Server ist leider nicht erreichbar!");
 		}
 		return distance;
+	}
+	
+	private Boolean connectInternet() {
+			try {
+				try {
+					URL url = new URL("http://www.google.de");
+					HttpURLConnection con = (HttpURLConnection) url.openConnection();
+					con.connect();
+					if (con.getResponseCode() == 200) {
+						return(true);
+					}
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "Kann keine Verbindung herstellen.\n Sorgen Sie für eine stabile Internetverbindung!", "Verbindungsfehler", JOptionPane.ERROR_MESSAGE);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
 	}
 
 }
