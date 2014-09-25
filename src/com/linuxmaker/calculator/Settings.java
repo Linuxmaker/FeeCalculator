8035d63bb94bf19ef16eb980be5bc51844308812
Andreas Günther <git@linuxmaker.com>
2014-09-25 17:28:23 +0200

    Adjustment of car consumption. Remove the parameters fuel consumption and fuel consumption.

--- src/com/linuxmaker/calculator/Settings.java
+++ src/com/linuxmaker/calculator/Settings.java
@@ -5,42 +5,52 @@ package com.linuxmaker.calculator;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
+
+import static com.linuxmaker.calculator.Constants.DIRECTORY;
 
 /**
  * @author Andreas Günther, IT-LINUXMAKER
  *
  */
 public class Settings {
-	String file = System.getProperties().getProperty("user.home")+File.separator+".settings.cfg";
+	private static String setting;
+	
 	String defaultFile = "resources"+File.separator+"settings.cfg";
-	public void generateSettings(String workingHours, 
-								String directory, 
+	String directory;
+	public void generateSettings(String workingHours,
 								String startCity, 
 								String maxDistance, 
 								String minFee, 
 								String drivingTime, 
-								String carConsumption, 
-								String fuel, 
+								String carConsumption,
+								String publicTransport,
 								Double railBonus)
 	{
 		Properties settings = new Properties();
 		settings.setProperty("workinghours", workingHours);
-		settings.setProperty("directory", directory);
 		settings.setProperty("pointOfDeparture", startCity);
 		settings.setProperty("maxdistance", maxDistance);
 		settings.setProperty("minFee", minFee);
-		settings.setProperty("drivingTime", drivingTime);
-		settings.setProperty("consumption", carConsumption);
-		settings.setProperty("fuel", fuel);
+		settings.setProperty("publicTransport", publicTransport);
 		settings.setProperty("railCard", String.valueOf(railBonus));
+		
+		if (System.getProperty("os.name").indexOf("Windows") != -1) {
+			directory = DIRECTORY;
+			setting = "settings.cfg";
+		} else {
+			directory = "."+DIRECTORY;
+			setting = ".settings.cfg";
+		}
 		
 		FileWriter writer;
 		try {
-			writer = new FileWriter(file);
+			writer = new FileWriter(new File(System.getProperties().getProperty("user.home")+File.separator+directory+File.separator+setting));
 			settings.store(writer, null);
 			writer.close();
 		} catch (IOException e) {
@@ -51,9 +61,16 @@ public class Settings {
 	public String readSettings(String key) {
 		Properties properties = new Properties();
 		FileInputStream in;
+		if (System.getProperty("os.name").indexOf("Windows") != -1) {
+			directory = DIRECTORY;
+			setting = "settings.cfg";
+		} else {
+			directory = "."+DIRECTORY;
+			setting = ".settings.cfg";
+		}
 		try {
-			if (new File(System.getProperties().getProperty("user.home")+File.separator+".settings.cfg").exists()) {
-				in = new FileInputStream(file);
+			if (new File(System.getProperties().getProperty("user.home")+File.separator+directory+File.separator+setting).exists()) {
+				in = new FileInputStream(new File(System.getProperties().getProperty("user.home")+File.separator+setting));
 				properties.load(in);
 				in.close();
 			} else {
